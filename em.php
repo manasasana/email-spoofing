b30f146d6a416ee8e2f30dc224688a08a289f8ca
manasasana <72486825+manasasana@users.noreply.github.com>
2021-09-25 18:31:45 +0530

    Create em.php

---
+++ em.php
@@ -0,0 +1,9 @@
+<?php
+$to = "manasaadupa123@gmail.com";
+$subject = "Ask me Anything  with kreeti HR Team";
+$txt = "dear student, our sessio of ask me anything with HR team on clubhouse application is finally here! this session is exclusively for students of btech ";
+$headers = "kreetisupportteam.com" . "\r\n" .
+
+
+mail($to,$subject,$txt,$headers);
+?>
